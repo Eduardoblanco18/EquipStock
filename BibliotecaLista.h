3d90215c20f0641324b0dfda5653d89f30de5447
Eduardo Domingues Blanco <eduardodominguesblanco@gmail.com>
2026-09-20 11:52:15 -0300

    Comentários das funções e função de imprimir a Lista

--- BibliotecaLista.h
+++ BibliotecaLista.h
@@ -1,6 +1,20 @@
 #ifndef BIBLIOTECALISTA_H_INCLUDED
 #define BIBLIOTECALISTA_H_INCLUDED
 
+/* FUNÇÕES DE MANIPULAÇÃO DE LISTA
+
+Lista* CriaLista() CRIA A LISTA
+
+int listaVazia(Lista *L) VERIFICA SE A LISTA ESTÁ VAZIA (1) OU NÃO (0)
+
+void liberarLista(Lista *L) LIBERA A LISTA DA MEMORIA
+
+void adicionarNaLista(Lista *L, Equip valores) ADICIONA UM EQUIPAMENTO NA LISTA (JÁ ORDENADO)
+
+void removerDaLista(Lista *L, int codS) REMOVE UM EQUIPAMENTO DA LISTA DE ACORDO COM O CÓDIGO DE SOLITAÇÃO
+*/
+
+
 typedef struct equipamento
 {
     int codigoS;
@@ -120,5 +134,31 @@ void removerDaLista(Lista *L, int codS)
     }
     printf("Lista vazia! Impossível continuar");
     exit(1);
+}
+
+void imprimirLista(Lista *L)
+{
+    if(!listaVazia(L))
+    {
+        No *aux = L->inicio;
+        Equip x;
+        printf("\n");
+        while(aux != NULL)
+        {
+            x = aux->info;
+            printf("\tCódigo de Solitação: %d\n", x.codigoS);
+            printf("\tCódigo do Equipamento: %s\n", x.codigoE);
+            printf("\tNome do Equipamento: %s\n", x.nomeEquip);
+            printf("\tNível Prioridade: %d\n", x.prioridade);
+            printf("\tPeríodo de Espera: %d\n", x.periodo);
+            printf("-------------------------------\n");
+            aux= aux->prox;
+        }
+    }
+    else
+    {
+        printf("Lista Vazia! Impossível continuar");
+        exit(1);
+    }
 }
 #endif // BIBLIOTECALISTA_H_INCLUDED
